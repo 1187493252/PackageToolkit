8282e3a0ce2dbf09c26314bae6c472195ba33b38
DESKTOP-V7TK7FL\admin <1187493252@qq.com>
2024-09-13 17:42:06 +0800

    luban

--- Editor/LuBan/LuBanPackage.cs
+++ Editor/LuBan/LuBanPackage.cs
@@ -12,13 +12,7 @@ namespace UnityFramework.Editor.Package
         [MenuItem("Assets/SCPackages/Data/LuBan_Unity ",false,1)]
         private  static void AddLuBanPackage()
         {
-            AddRequest addRequest = Client.Add(Toolkit.GetPackageUrl("luban_unity_git"));
-            if (addRequest.Status == StatusCode.Failure)
-            {
-                Client.Add(Toolkit.GetPackageUrl("luban_unity_gitee"));
-            }
-            return;
-         addRequest = Client.Add($"https://github.com/focus-creative-games/luban_unity.git");
+       var  addRequest = Client.Add($"https://github.com/focus-creative-games/luban_unity.git");
             if (addRequest.Status== StatusCode.Failure)
             {
             Client.Add($"https://gitee.com/focus-creative-games/luban_unity.git");
--- Editor/Toolkit.cs
+++ Editor/Toolkit.cs
@@ -5,33 +5,5 @@ using UnityEngine;
 
 public static class Toolkit 
 {
-     static Dictionary<string,string> packageUrlData=new Dictionary<string,string>();
-    public static string GetPackageUrl(string key)
-    { 
-        if (packageUrlData.Count==0)
-        {
-            LoadPackageUrlData();
-        }
-        return packageUrlData[key];
-    }
-    public static void LoadPackageUrlData()
-    {
-        var jsondata = AssetDatabase.LoadAssetAtPath<TextAsset>($"Packages/com.sc.packages/PackageUrl.json");
-        PackageUrlData  data=    JsonUtility.FromJson<PackageUrlData>(jsondata.text);
-        foreach (Config config in data.config)
-        {
-            packageUrlData[config.key]=config.value;
-        }
-    }
-
-    class PackageUrlData
-    {
-        public Config[] config;
-    }
-
-    class Config
-    {
-        public string key;
-        public string value;
-    }
+ 
 }

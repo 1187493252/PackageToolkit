3b3c878f563a3b034de91a4559f12566b558302d
DESKTOP-V7TK7FL\admin <1187493252@qq.com>
2024-09-13 17:32:19 +0800

    no message

--- Editor/LuBan/LuBanPackage.cs
+++ Editor/LuBan/LuBanPackage.cs
@@ -1,11 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting.FullSerializer;
 using UnityEditor;
 using UnityEditor.PackageManager;
 using UnityEditor.PackageManager.Requests;
 using UnityEngine;
-using static Toolkit;
 
 namespace UnityFramework.Editor.Package
 {

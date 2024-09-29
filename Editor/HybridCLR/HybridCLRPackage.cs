e27c319edf7bdeda404c15e8c12ff2dbb3adb11a
DESKTOP-V7TK7FL\admin <1187493252@qq.com>
2024-09-29 10:48:51 +0800

    宏定义

--- Editor/HybridCLR/HybridCLRPackage.cs
+++ Editor/HybridCLR/HybridCLRPackage.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using UnityEditor;
 using UnityEditor.PackageManager;
 
@@ -5,15 +6,16 @@ namespace UnityFramework.Editor.Package
 {
     public partial class UnityEditorTools
     {
-        [MenuItem("Assets/SCPackages/HotUpdate/HybridCLR ", false,1)]
-        private  static void AddHybridCLRPackage()
+        [MenuItem("Assets/SCPackages/HotUpdate/HybridCLR ", false, 1)]
+        private static void AddHybridCLRPackage()
         {
-       var  addRequest = Client.Add($"https://github.com/focus-creative-games/hybridclr_unity.git");
-            if (addRequest.Status== StatusCode.Failure)
+            var addRequest = Client.Add($"https://github.com/focus-creative-games/hybridclr_unity.git");
+            if (addRequest.Status == StatusCode.Failure)
             {
-            Client.Add($"https://gitee.com/focus-creative-games/hybridclr_unity.git");
+                Client.Add($"https://gitee.com/focus-creative-games/hybridclr_unity.git");
             }
         }
 
     }
 }
+#endif
--- Editor/LuBan/LuBanPackage.cs
+++ Editor/LuBan/LuBanPackage.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using UnityEditor;
 using UnityEditor.PackageManager;
 
@@ -5,15 +6,16 @@ namespace UnityFramework.Editor.Package
 {
     public partial class UnityEditorTools
     {
-        [MenuItem("Assets/SCPackages/Data/LuBan_Unity ",false,1)]
-        private  static void AddLuBanPackage()
+        [MenuItem("Assets/SCPackages/Data/LuBan_Unity ", false, 1)]
+        private static void AddLuBanPackage()
         {
-       var  addRequest = Client.Add($"https://github.com/focus-creative-games/luban_unity.git");
-            if (addRequest.Status== StatusCode.Failure)
+            var addRequest = Client.Add($"https://github.com/focus-creative-games/luban_unity.git");
+            if (addRequest.Status == StatusCode.Failure)
             {
-            Client.Add($"https://gitee.com/focus-creative-games/luban_unity.git");
+                Client.Add($"https://gitee.com/focus-creative-games/luban_unity.git");
             }
         }
 
     }
 }
+#endif
